c176bf94c31e4813abd1e76908edf1af7225082b
katlong0709 <katlong0709@gmail.com>
2023-02-22 19:37:56 +0800

    major changes
    ported gameobjinstance functionality into the project

--- GAME2/Level1.cpp
+++ GAME2/Level1.cpp
@@ -7,7 +7,6 @@
 
 AEGfxTexture* floorTex; // Pointer to floor image texture
 AEGfxTexture* hole_big;
-AEGfxTexture* playerTex;
 AEGfxTexture* hole_small;
 AEGfxTexture* hole_none;
 AEGfxTexture* slimeTexture;
@@ -35,7 +34,6 @@ AEVec2 pt8 = { 250.0f, 200.0f };
 
 
 //float obj1X = 0.0f, obj1Y = 0.0f; // Position variables for object 1
-float objtexX, objtexY; // Texture variables for object 2 and 3 texture
 
 bool jump;
 bool falling;
@@ -63,13 +61,159 @@ AEVec2 test = { 0.f, 1.f };
 
 int counter = 0; // Counter to swap textures
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/******************************************************************************/
+/*!
+	Defines
+*/
+/******************************************************************************/
+const unsigned int	GAME_OBJ_NUM_MAX = 32;					// The total number of different objects (Shapes)
+const unsigned int	GAME_OBJ_INST_NUM_MAX = 2048;			// The total number of different game object instances
 
 
+const unsigned int	SHIP_INITIAL_NUM = 3;			// initial number of ship lives
+const float			PLAYER_SIZE = 60.0f;					// player size
+const float			SHIP_ACCEL_FORWARD = 40.0f;		// ship forward acceleration (in m/s^2)
+const float			SHIP_ACCEL_BACKWARD = 40.0f;		// ship backward acceleration (in m/s^2)
+const float			SHIP_ROT_SPEED = (2.0f * PI);	// ship rotation speed (degree/second)
 
+
+const float			BULLET_SIZE = 12.0f;			// bullet size (self added)
+const float			BULLET_SPEED = 100.0f;		// bullet speed (m/s)
+
+const float         BOUNDING_RECT_SIZE = 1.0f;         // this is the normalized bounding rectangle (width and height) sizes - AABB collision data
+
+// -----------------------------------------------------------------------------
+enum TYPE
+{
+	// list of game object types
+	TYPE_PLAYER = 0,
+	TYPE_BULLET,
+	TYPE_ASTEROID,
+
+	TYPE_NUM
+};
+
+// -----------------------------------------------------------------------------
+// object flag definition
+
+const unsigned long FLAG_ACTIVE = 0x00000001;
+
+/******************************************************************************/
+/*!
+	Struct/Class Definitions
+*/
+/******************************************************************************/
+
+//Game object structure
+struct GameObj
+{
+	unsigned long		type;		// object type
+	AEGfxVertexList*	pMesh;		// This will hold the triangles which will form the shape of the object
+};
+
+// ---------------------------------------------------------------------------
+
+//Game object instance structure
+struct GameObjInst
+{
+	GameObj*			pObject;	// pointer to the 'original' shape
+	unsigned long		flag;		// bit flag or-ed together
+	float				scale;		// scaling value of the object instance
+	AEVec2				posCurr;	// object current position
+	AEVec2				velCurr;	// object current velocity
+	float				dirCurr;	// object current direction
+	AABB				boundingBox;// object bouding box that encapsulates the object
+	AEMtx33				transform;	// object transformation matrix: Each frame, 
+									// calculate the object instance's transformation matrix and save it here
+
+	//void				(*pfUpdate)(void);
+	//void				(*pfDraw)(void);
+};
+
+/******************************************************************************/
+/*!
+	Static Variables
+*/
+/******************************************************************************/
+
+// list of original object
+static GameObj				sGameObjList[GAME_OBJ_NUM_MAX];				// Each element in this array represents a unique game object (shape)
+static unsigned long		sGameObjNum;								// The number of defined game objects
+
+// list of object instances
+static GameObjInst			sGameObjInstList[GAME_OBJ_INST_NUM_MAX];	// Each element in this array represents a unique game object instance (sprite)
+static unsigned long		sGameObjInstNum;							// The number of used game object instances
+
+// pointer to the ship object
+static GameObjInst* spPlayer;										// Pointer to the "Ship" game object instance
+
+// number of ship available (lives 0 = game over)
+static long					sShipLives;									// The number of lives left
+
+// the score = number of asteroid destroyed
+static unsigned long		sScore;										// Current score
+
+// flag to trigger printing
+static bool					onValueChange;								// Printing flag
+static bool					win;										// Win flag
+
+// timer to trigger spawn
+static float				timer_spawn;								// Spawn timer
+
+// textures declaration
+static AEGfxTexture*		playerTex;									// Player texture
+
+// ---------------------------------------------------------------------------
+
+// functions to create/destroy a game object instance
+GameObjInst* gameObjInstCreate(unsigned long type, float scale,
+	AEVec2* pPos, AEVec2* pVel, float dir);
+void					gameObjInstDestroy(GameObjInst* pInst);
+/// <summary>
+/// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// </summary>
 
 void Level1_Load()
 {
-	//////////////////////////////////
+	// zero the game object array
+	memset(sGameObjList, 0, sizeof(GameObj) * GAME_OBJ_NUM_MAX);
+	// No game objects (shapes) at this point
+	sGameObjNum = 0;
+
+	// zero the game object instance array
+	memset(sGameObjInstList, 0, sizeof(GameObjInst) * GAME_OBJ_INST_NUM_MAX);
+	// No game object instances (sprites) at this point
+	sGameObjInstNum = 0;
+
+	// The ship object instance hasn't been created yet, so this "spShip" pointer is initialized to 0
+	spPlayer = nullptr;
+
+	// load/create the mesh data (game objects / Shapes)
+	GameObj* pObj;
+
+
+	// =====================
+	// create the ship shape
+	// =====================
+
+	pObj = sGameObjList + sGameObjNum++;
+	pObj->type = TYPE_PLAYER;
+
+	AEGfxMeshStart();
+	AEGfxTriAdd(
+		-0.5f, 0.5f, 0x00FF0FF, 1.0f, 1.0f,
+		-0.5f, -0.5f, 0x00FFFFFF, 0.0f, 1.0f,
+		0.5, -0.5f, 0x0000FFFF, 0.0f, 0.0f);
+
+	AEGfxTriAdd(
+		-0.5f, 0.5f, 0x00FFFFFF, 1.0f, 1.0f,
+		0.5f, 0.5f, 0x00FFFFFF, 1.0f, 0.0f,
+		0.5f, -0.5f, 0x00FFFFFF, 0.0f, 0.0f);
+
+	pObj->pMesh = AEGfxMeshEnd();
+	AE_ASSERT_MESG(pObj->pMesh, "fail to create square box mesh!!");
+	/////////////////////////////////////////////////////////////////
 
 
 
@@ -146,7 +290,6 @@ void Level1_Load()
 	hole_big = AEGfxTextureLoad("Assets/hole_big.png");
 	hole_small = AEGfxTextureLoad("Assets/hole_small.png");
 	hole_none = AEGfxTextureLoad("Assets/hole_none.png");
-	playerTex = AEGfxTextureLoad("Assets/player_cube.png");
 	AE_ASSERT_MESG(floorTex, "Failed to create texture1!!");
 
 
@@ -159,10 +302,24 @@ void Level1_Load()
 	//////////////////////////////////
 
 
+
+
+	// =========================
+	// load the player texture
+	// =========================
+	playerTex = AEGfxTextureLoad("Assets/player_cube.png");
+	AE_ASSERT_MESG(playerTex, "Failed to create player texture!!");
 }
 
 void Level1_Initialize()
 {
+	// Create the main ship
+	AEVec2 init_ship_pos = { 0.f, 0.f };
+	AEVec2 init_ship_vel = { 0.f, 0.f };
+	spPlayer = gameObjInstCreate(TYPE_PLAYER, PLAYER_SIZE, &init_ship_pos, &init_ship_vel, PI / 2.f);
+	AE_ASSERT(spPlayer);
+
+
 	/*
 	AEVec2Set(&pt1, -250.f, 200.0f);
 	AEVec2Set(&pt2, -250.f, 170.0f);
@@ -176,8 +333,6 @@ void Level1_Initialize()
 
 	slime_pos.x = 110.0f;
 	slime_pos.y = -130.0f;
-	objtexX = 0;
-	objtexY = 0;
 
 	jump_timer = 0.0f;
 
@@ -373,21 +528,83 @@ void Level1_Update()
 
 
 
-	// Texture offset
-	if (AEInputCheckCurr(AEVK_L))
-		objtexX -= 0.01f;
-	else if (AEInputCheckCurr(AEVK_J))
-		objtexX += 0.01f;
-
-	if (AEInputCheckCurr(AEVK_I))
-		objtexY += 0.01f;
-	else if (AEInputCheckCurr(AEVK_K))
-		objtexY -= 0.01f;
 
 	updatePos(slime_pos.x, slime_pos.y);
 	AEGfxSetCamPosition(obj1.x, obj1.y);
 	// Game loop update end
-	///////////////////////
+	////////////////////////
+
+
+	
+
+
+	// Constant update of Player position from its velocity
+	AEVec2 shipnewvel;
+	AEVec2Scale(&shipnewvel, &spPlayer->velCurr, g_dt);
+	AEVec2Add(&spPlayer->posCurr, &spPlayer->posCurr, &shipnewvel);
+
+
+	// ======================================================
+	// update physics of all active game object instances
+	//  -- Get the AABB bounding rectangle of every active instance:
+	//		boundingRect_min = -(BOUNDING_RECT_SIZE/2.0f) * instance->scale + instance->pos
+	//		boundingRect_max = +(BOUNDING_RECT_SIZE/2.0f) * instance->scale + instance->pos
+	//
+	//	-- Positions of the instances are updated here with the already computed velocity (above)
+	// ======================================================
+	for (unsigned long i = 0; i < GAME_OBJ_INST_NUM_MAX; i++)
+	{
+		// Check if current instance is used
+		if (sGameObjInstList[i].flag == FLAG_ACTIVE)
+		{
+			// Computes the AABB bounding rectangle of instances
+			sGameObjInstList[i].boundingBox.min.x = -(BOUNDING_RECT_SIZE / 2.f) * sGameObjInstList[i].scale + sGameObjInstList[i].posCurr.x;
+			sGameObjInstList[i].boundingBox.min.y = -(BOUNDING_RECT_SIZE / 2.f) * sGameObjInstList[i].scale + sGameObjInstList[i].posCurr.y;
+			sGameObjInstList[i].boundingBox.max.x = (BOUNDING_RECT_SIZE / 2.f) * sGameObjInstList[i].scale + sGameObjInstList[i].posCurr.x;
+			sGameObjInstList[i].boundingBox.max.y = (BOUNDING_RECT_SIZE / 2.f) * sGameObjInstList[i].scale + sGameObjInstList[i].posCurr.y;
+
+			// Update positions of instances with computed velocity
+			AEVec2 newvel;
+			AEVec2Scale(&newvel, &sGameObjInstList[i].velCurr, g_dt);
+			AEVec2Add(&sGameObjInstList[i].posCurr, &sGameObjInstList[i].posCurr, &newvel);
+		}
+	}
+
+
+
+
+
+
+	// =====================================
+	// calculate the matrix for all objects
+	// =====================================
+
+	for (unsigned long i = 0; i < GAME_OBJ_INST_NUM_MAX; i++)
+	{
+		GameObjInst* pInst = sGameObjInstList + i;
+		AEMtx33		 trans, rot, scale;
+
+		trans = rot = scale = { 0 };
+
+		UNREFERENCED_PARAMETER(trans);
+		UNREFERENCED_PARAMETER(rot);
+		UNREFERENCED_PARAMETER(scale);
+
+		// skip non-active object
+		if ((pInst->flag & FLAG_ACTIVE) == 0)
+			continue;
+
+		// Compute the scaling matrix
+		AEMtx33Scale(&scale, pInst->scale, pInst->scale);
+		// Compute the rotation matrix 
+		AEMtx33Rot(&rot, pInst->dirCurr);
+		// Compute the translation matrix
+		AEMtx33Trans(&trans, pInst->posCurr.x, pInst->posCurr.y);
+		// Concatenate the 3 matrix in the correct order in the object instance's "transform" matrix
+		AEMtx33Concat(&(pInst->transform), &rot, &scale);
+		AEMtx33Concat(&(pInst->transform), &trans, &pInst->transform);
+
+	}
 }
 
 void Level1_Draw()
@@ -607,12 +824,60 @@ void Level1_Draw()
 	AEGfxSetTransform(transform.m);
 	AEGfxMeshDraw(pMeshRect, AE_GFX_MDM_TRIANGLES);
 	/// <Draw Timer Bar end>
-	
+
+
+	/// <summary>
+	/// ///////////////////////////////////////////////////////////////////////////////////////////////////
+	/// </summary>
+	char strBuffer[1024];
+
+	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+	AEGfxTextureSet(NULL, 0, 0);
+
+
+	// draw all object instances in the list
+	for (unsigned long i = 0; i < GAME_OBJ_INST_NUM_MAX; i++)
+	{
+		GameObjInst* pInst = sGameObjInstList + i;
+
+		// skip non-active object
+		if ((pInst->flag & FLAG_ACTIVE) == 0)
+			continue;
+
+
+		if (pInst->pObject->type == TYPE_PLAYER)
+		{
+			// Set texture for all asteroid object
+			AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
+			AEGfxSetBlendMode(AE_GFX_BM_BLEND);
+			AEGfxSetTransparency(1.0f);
+			AEGfxTextureSet(playerTex, 0.f, 0.f);
+		}
+		else
+		{
+			// No texture for everything else
+			AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+			AEGfxTextureSet(NULL, 0.f, 0.f);
+		}
+
+
+		// Set the current object instance's transform matrix using "AEGfxSetTransform"
+		AEGfxSetTransform(pInst->transform.m);
+		// Draw the shape used by the current object instance using "AEGfxMeshDraw"
+		AEGfxMeshDraw(pInst->pObject->pMesh, AE_GFX_MDM_TRIANGLES);
+	}
+	//////////////////////////////////////////////////////////////////////////////////////
 }
 
 void Level1_Free()
 {
-	
+	// kill all object instances in the array using "gameObjInstDestroy"
+	for (unsigned long i = 0; i < GAME_OBJ_INST_NUM_MAX; i++)
+	{
+		gameObjInstDestroy(sGameObjInstList + i);
+	}
+
+	gameObjInstDestroy(spPlayer);
 }
 
 void Level1_Unload()
@@ -630,4 +895,75 @@ void Level1_Unload()
 	AEGfxMeshFree(pMeshLine);
 	AEGfxMeshFree(pMeshBox);
 	AEGfxMeshFree(pMeshRect);
+
+
+
+	// free all mesh data (shapes) of each object using "AEGfxTriFree"
+	while (--sGameObjNum != 0)
+	{
+		AEGfxMeshFree((sGameObjList + sGameObjNum)->pMesh);
+	}
+	AEGfxMeshFree((sGameObjList + sGameObjNum)->pMesh);
+	AEGfxTextureUnload(playerTex);
+}
+
+
+
+
+
+
+/******************************************************************************/
+/*!
+	Object Instance create function of this state
+*/
+/******************************************************************************/
+GameObjInst* gameObjInstCreate(unsigned long type,
+	float scale,
+	AEVec2* pPos,
+	AEVec2* pVel,
+	float dir)
+{
+	AEVec2 zero;
+	AEVec2Zero(&zero);
+
+	AE_ASSERT_PARM(type < sGameObjNum);
+
+	// loop through the object instance list to find a non-used object instance
+	for (unsigned long i = 0; i < GAME_OBJ_INST_NUM_MAX; i++)
+	{
+		GameObjInst* pInst = sGameObjInstList + i;
+
+		// check if current instance is not used
+		if (pInst->flag == 0)
+		{
+			// it is not used => use it to create the new instance
+			pInst->pObject = sGameObjList + type;
+			pInst->flag = FLAG_ACTIVE;
+			pInst->scale = scale;
+			pInst->posCurr = pPos ? *pPos : zero;
+			pInst->velCurr = pVel ? *pVel : zero;
+			pInst->dirCurr = dir;
+
+			// return the newly created instance
+			return pInst;
+		}
+	}
+
+	// cannot find empty slot => return 0
+	return 0;
+}
+
+/******************************************************************************/
+/*!
+	Object Instance destroy function of this state
+*/
+/******************************************************************************/
+void gameObjInstDestroy(GameObjInst* pInst)
+{
+	// if instance is destroyed before, just return
+	if (pInst->flag == 0)
+		return;
+
+	// zero out the flag
+	pInst->flag = 0;
 }
